cb9192ace3757c7714f89a934e1f05dbcf494db6
Andres Ramos <andres@arsoft.net>
2023-04-25 15:50:02 -0500

    add IApplicationDbContext

---
+++ src/ARSoftware.Contpaqi.Api.Common/Persistence/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ARSoftware.Contpaqi.Api.Common.Persistence;
+
+public interface IApplicationDbContext
+{
+    DbSet<ApiRequest> Requests { get; }
+    DbSet<ApiResponse> Responses { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
